b23af25ea6ce46c7662d43e8c9111e5f0523b848
Tom <thomas.spencer@codurance.com>
2021-11-26 15:32:30 +0000

    Add naive implementation for reduce with adding for franc and dollar

---
+++ src/main/java/com/codurance/Bank.java
@@ -0,0 +1,8 @@
+package com.codurance;
+
+public class Bank {
+
+  public Money reduce(Expression source, String to) {
+    return Money.dollar(10);
+  }
+}
--- src/test/java/com/codurance/MoneyTest.java
+++ src/test/java/com/codurance/MoneyTest.java
@@ -16,8 +16,11 @@ public class MoneyTest {
 
   @Test
   void testSimpleAddition() {
-    final Money sum = Money.dollar(5).plus(Money.dollar(5));
-    assertEquals(Money.dollar(10), sum);
+    final Money five = Money.dollar(5);
+    final Expression sum = five.plus(five);
+    final Bank bank = new Bank();
+    Money reduced = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(10), reduced);
   }
 
   @Test
