9baa513ccfd4d8a626ba6e2dc08e1bf0185d52e5
Tom <thomas.spencer@codurance.com>
2021-11-26 15:16:30 +0000

    Simple multiplication

--- src/test/java/com/codurance/MoneyTest.java
+++ src/test/java/com/codurance/MoneyTest.java
@@ -15,6 +15,12 @@ public class MoneyTest {
   }
 
   @Test
+  void testSimpleAddition() {
+    final Money sum = Money.dollar(5).plus(Money.dollar(5));
+    assertEquals(Money.dollar(10), sum);
+  }
+
+  @Test
   void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
